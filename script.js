6d07c92f022d1011f15f7306633be5626a1ba206
Walter Reis <xinchro@xinchronize.com>
2020-06-06 19:39:26 +0100

    added basic disc visualizer

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ let canvasCtx
 let audioStream
 let frame = 0
 let heightMultiplier = 10
-let fftSize = 64
+let fftSize = 128
 let decay = 0.5 // higher is slower
 let groupWidth = 1.0 // higher groups more frequencies to the same visual bar
+let barWidth
+let focusPoint = false
 
 navigator.mediaDevices.getUserMedia({ audio: true })
   .then(startStream)
@@ -21,7 +23,7 @@ function showError(error) {
 
 function startStream(stream) {
   audioStream = stream
-  updateFftSize(fftSize)
+  setFftSize(fftSize)
 
   canvas = document.getElementById('visualizer')
   canvasCtx = canvas.getContext('2d')
@@ -37,7 +39,7 @@ function startStream(stream) {
   draw()
 }
 
-function updateFftSize(newSize) {
+function setFftSize(newSize) {
   analyser.fftSize = newSize
   bufferLength = analyser.frequencyBinCount
   dataArray = new Float32Array(bufferLength)
@@ -57,13 +59,105 @@ function draw() {
   canvasCtx.beginPath()
 
   groupWidth = groupWidth < bufferLength ? groupWidth : bufferLength
-  console.log(bufferLength)
 
-  let barWidth = canvas.width * groupWidth / bufferLength
+  // drawSingleVariant()
+  // drawQuadVariant()
+  circleVarient()
+
+  frame++
+}
+
+function circleVarient() {
+  let radius = 100
+  let circumference = 2 * Math.PI * radius
+  let totalDisc = 2 * Math.PI
+  let maxBarHeight = (canvas.height - (radius*2))/2
+
+  for (let i = 0; i < bufferLength; i++) {
+    let x1 = 0
+    let y1 = 0
+    let x2 = 0
+    let y2 = 0
+    let volume = 128 - Math.abs(dataArray[i])
+    let percentage = i / bufferLength
+    let angle = totalDisc * percentage
+
+    let segmentLength = circumference / bufferLength
+
+    let posx = Math.cos(angle)
+    let posy = Math.sin(angle)
+
+    y1 = canvas.height - ((volume) * heightMultiplier)/2 // 128 is oscilloscope 0 (center)
+
+    x1 = canvas.width/2 + posx * radius
+    y1 = canvas.height/2 + posy * radius
+    x2 = segmentLength
+    y2 = ((volume) * heightMultiplier)/2
+
+    tx1 = canvas.width/4 + Math.cos(i) * radius
+    ty1 = canvas.height/4 + Math.sin(i) * radius
+
+    if(y2 <= segmentLength) {
+      y2 = segmentLength
+    }
+    if(y2 >= maxBarHeight) {
+      y2 = maxBarHeight
+    }
+
+    canvasCtx.fillStyle = 'hsla(' + (i*(360/bufferLength)+frame) + ',100%,50%,50%)'
+    canvasCtx.translate(canvas.width/2, canvas.height/2)
+    canvasCtx.rotate(angle - Math.PI)
+    canvasCtx.fillRect(0, radius, x2, y2)
+    canvasCtx.setTransform(1, 0, 0, 1, 0, 0)
+  }
+}
+
+function drawQuadVariant() {
+  barWidth = canvas.width * groupWidth / bufferLength
+  let xOffset = 0
+
+  for (let i = 0; i < bufferLength; i++) {
+    let x1 = 0
+    let y1 = 0
+    let x2 = 0
+    let y2 = 0
+    let volume = 0
+
+    volume = 128 - Math.abs(dataArray[i])
+
+    y1 = canvas.height - ((volume) * heightMultiplier)/2 // 128 is oscilloscope 0 (center)
+    // if(y2 <= 0) y1 = 0
+
+    x1 = x1+canvas.width/2
+    y1 = y1/2
+    x2 = barWidth/2
+    y2 = (canvas.height/2 - y1)*2
+
+    if(y2 <= 0) {
+      y2 = 10
+      y1 = canvas.height/2 - y2/2
+    }
+
+    // right side
+    canvasCtx.fillStyle = 'hsla(' + (i*(360/bufferLength)+frame) + ',100%,50%,50%)'
+    canvasCtx.fillRect(x1+xOffset, y1, x2, y2) // top right
+
+    //left side
+    if(focusPoint) {
+      canvasCtx.fillStyle = 'hsla(' + (i*(360/bufferLength)+frame) + ',100%,50%,50%)'
+    } else {
+      canvasCtx.fillStyle = 'hsla(' + (frame-i*(360/bufferLength)) + ',100%,50%,50%)'
+    }
+    // don't render first bar on the left, to not overlap with right
+    if(i!=0) canvasCtx.fillRect(x1-xOffset, y1, x2, y2) // top left
+
+    xOffset += barWidth/2
+  }
+}
+
+function drawSingleVariant() {
+  barWidth = canvas.width * groupWidth / bufferLength
   let x = 0
-
-  document.getElementById('feedback') ? document.getElementById('feedback').innerText = dataArray[0] : ''
-
   for (let i = 0; i < bufferLength; i++) {
 
     let y = canvas.height - ((128 - Math.abs(dataArray[i])) * heightMultiplier) // 128 is oscilloscope 0 (center)
@@ -74,6 +168,4 @@ function draw() {
 
     x += barWidth
   }
-
-  frame++
 }
